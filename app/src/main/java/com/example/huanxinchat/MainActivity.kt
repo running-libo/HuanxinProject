a77441ed48a45b4b1396fdca2672b55524a148ba
apple <18201497620@163.com>
2020-10-09 22:10:58 +0800

    环信注册登录成功

--- app/src/main/java/com/example/huanxinchat/MainActivity.kt
+++ app/src/main/java/com/example/huanxinchat/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.huanxinchat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
 import android.util.Log
 import com.hyphenate.EMCallBack
 import com.hyphenate.chat.EMClient
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BasePermissionActivity() {
     val userName = "YXA6TYDA_OZEQniTrMuCK_z8gQ"
     val psd = "YXA6AuYDeAKIx4hwsIOjVv41MdXen5E"
 
@@ -14,11 +14,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //注册账号,注册失败会抛出HyphenateException: Registration failed.
-        EMClient.getInstance().createAccount(userName, psd)  //同步方法
+        requestPermissions()
+    }
 
-        //账号登录
-        login()
+    /**
+     * App权限申请
+     */
+    private fun requestPermissions() {
+        val permissions = arrayOf(
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA
+        )
+
+        requestPermissions(permissions, object : PermissionListener {
+            override fun onGranted() {
+                imRegisterAndLogin()
+            }
+
+            override fun onDenied(deniedPermissions: MutableList<String>?) {
+
+            }
+
+        })
+    }
+
+    fun imRegisterAndLogin() {
+        //注意：这里的注册有个坑，必须要在子线程中才可以，否则会抛出异常
+        Thread {
+            kotlin.run {
+                //注册账号,注册失败会抛出HyphenateException: Registration failed.
+                EMClient.getInstance().createAccount(userName, psd)  //同步方法
+                runOnUiThread {
+                    //账号登录
+                    login()
+                }
+            }
+        }.start()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     fun login() {
