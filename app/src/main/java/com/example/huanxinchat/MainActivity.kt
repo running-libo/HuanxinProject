835dfbe2a70bd933ff75bf6fb8b648b5180b917d
apple <18201497620@163.com>
2020-10-09 18:28:23 +0800

    环信注册登录加入

--- app/src/main/java/com/example/huanxinchat/MainActivity.kt
+++ app/src/main/java/com/example/huanxinchat/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.huanxinchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.hyphenate.EMCallBack
+import com.hyphenate.chat.EMClient
 
 class MainActivity : AppCompatActivity() {
+    val userName = "YXA6TYDA_OZEQniTrMuCK_z8gQ"
+    val psd = "YXA6AuYDeAKIx4hwsIOjVv41MdXen5E"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //注册账号,注册失败会抛出HyphenateException: Registration failed.
+        EMClient.getInstance().createAccount(userName, psd)  //同步方法
+
+        //账号登录
+        login()
     }
+
+    fun login() {
+        EMClient.getInstance().login(userName, psd, object: EMCallBack {
+            override fun onSuccess() {
+                Log.i("minfo", "登录聊天服务器成功")
+            }
+
+            override fun onProgress(progress: Int, status: String?) {
+
+            }
+
+            override fun onError(code: Int, error: String?) {
+                Log.i("minfo", "登录聊天服务器失败")
+            }
+
+        })
+    }
+
 }
---
+++ app/src/main/java/com/example/huanxinchat/MyApp.kt
@@ -0,0 +1,35 @@
+package com.example.huanxinchat
+
+import android.app.Application
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMOptions
+
+/**
+ * create by libo
+ * create on 2020/10/9
+ * description
+ */
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        initIm()
+    }
+
+    private fun initIm() {
+        var options = EMOptions()
+        // 默认添加好友时，是不需要验证的，改成需要验证
+        options.acceptInvitationAlways = false
+        // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
+        options.autoTransferMessageAttachments = true
+        // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
+        options.setAutoDownloadThumbnail(true)
+        //初始化
+        EMClient.getInstance().init(applicationContext, options)
+        //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        EMClient.getInstance().setDebugMode(true)
+
+    }
+
+}
